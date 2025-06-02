e0287fbfd5af627559e5ea1a3ee6847535f6ec3d
ayeuhugyu <planetaryrotationwastaken1@gmail.com>
2025-06-02 00:27:26 -0700

    hopefully NOWWW the version shit works

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,23 +1,6 @@
 import { Command, registerCommand } from "../lib/command";
 import chalk from "chalk";
-import fs from "fs-extra";
-import path from "path";
-
-// Use build-time injected version if available
-const BUILT_VERSION = typeof process !== 'undefined' && process.env.MINEPACK_VERSION ? process.env.MINEPACK_VERSION : undefined;
-
-function getVersion() {
-    if (BUILT_VERSION) return BUILT_VERSION;
-    // Try to read from package.json (dev mode only)
-    try {
-        const pkgPath = path.resolve(__dirname, "../../package.json");
-        if (fs.existsSync(pkgPath)) {
-            const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
-            return pkg.version || "unknown";
-        }
-    } catch {}
-    return "unknown";
-}
+import { VERSION } from "../version";
 
 const versionCommand = new Command({
     name: "version",
@@ -25,8 +8,7 @@ const versionCommand = new Command({
     arguments: [],
     flags: [],
     async execute() {
-        const version = getVersion();
-        console.log(chalk.green(`v${version}`));
+        console.log(chalk.green(`v${VERSION}`));
     }
 });
 
