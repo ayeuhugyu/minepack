c81dc32d24f8ef727036c5c6faeaf1d8d9ff1f8e
ayeuhugyu <72027312+ayeuhugyu@users.noreply.github.com>
2025-09-18 20:40:27 +0000

    searchAll function

---
+++ cmd/search.go
@@ -0,0 +1,73 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"minepack/core/api"
+	"minepack/core/project"
+
+	"github.com/spf13/cobra"
+)
+
+// searchCmd represents the search command
+var searchCmd = &cobra.Command{
+	Use:   "search",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide a search query.")
+			return
+		}
+		// query is all args joined with space
+		query := ""
+		for i, arg := range args {
+			if i > 0 {
+				query += " "
+			}
+			query += arg
+		}
+		fmt.Printf("Searching for: %s\n", query)
+		var templateProject = project.Project{
+			DefaultSource: "modrinth",
+			Versions: project.ProjectVersions{
+				Game: "1.20.1",
+				Loader: project.ModloaderVersion{
+					Name:   "fabric",
+				},
+			},
+		}
+		result, err := api.SearchAll(query, templateProject)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if result == nil {
+			fmt.Printf("No results found.")
+			return
+		}
+		fmt.Printf("Found: %s (%d) - %s\n", result.Name, result.Source, result.PageUrl)
+	},
+}
+
+func init() {
+	testCmd.AddCommand(searchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
